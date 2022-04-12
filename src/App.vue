631a9a3a7d9de7198f95c056f94c35f35e9a31f4
Demian Romanenko <dem.romanenko@gmail.com>
2022-04-12 23:53:56 +0300

    added slicing and unloading as an archive

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 import * as THREE from "three";
 import { GUI } from "lil-gui";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
@@ -18,31 +20,131 @@ let scene;
 let geometry;
 let object;
 let planes, planeObjects, planeHelpers;
+
+const gui = new GUI();
+
+// let strDownloadMime = "image/octet-stream";
 // let clock;
 const params = {
-  animate: false,
   planeY: {
     constant: 0,
     negated: false,
     displayHelper: true,
   },
   scale: 0.03,
+  // editVue: function () {
+  //   mode.slicing = !mode.slicing;
+  //   this.setCamera();
+  // },
+};
+
+const mode = {
+  slicing: false,
 };
 
 export default {
   mounted: async function () {
     geometry = await this.loadGeometry();
     geometry.scale(params.scale, params.scale, params.scale);
+    this.setGUI();
     this.init();
     window.addEventListener("resize", this.onWindowResize);
-    new OrbitControls(camera, renderer.domElement);
   },
   // TODO 100 добавить vite.config.ts (перейти на vite) tsconfig.json .eslintrc.js
   methods: {
+    loadGeometry: async function () {
+      return await loader.loadAsync(
+        "models/Model_44_S3.540.45_T3.8.46_E4.3.47_R5.7.stl"
+      );
+    },
+
+    sleep: function (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    },
+
+    pad: function (number, number_digits) {
+      return ("000000000" + number).slice(-number_digits);
+    },
+
+    saveImage: async function () {
+      const zip = new JSZip();
+      gui.hide();
+      const progress_gui = new GUI();
+      progress_gui
+        .add(planes[0], "constant")
+        .min(-1)
+        .max(1)
+        .name("progress")
+        .listen();
+      progress_gui.show();
+
+      const number_slice = 100;
+      for (let i = -number_slice; i <= number_slice; i++) {
+        planes[0].constant = (1 / number_slice) * i;
+        progress_gui.save();
+        await this.sleep(100);
+
+        const canvas = document.getElementsByTagName("canvas")[0];
+        let image = new Image();
+        image.src = canvas.toDataURL();
+        zip.file(
+          "slice" + this.pad(number_slice + i, 3) + ".png",
+          image.src.slice(image.src.indexOf(",") + 1),
+          { base64: true }
+        );
+      }
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        saveAs(content, "slicing.zip");
+      });
+      planes[0].constant = 0;
+      progress_gui.hide();
+      gui.show();
+    },
+
+    setGUI: function () {
+      const slicing = gui.addFolder("slicing");
+      slicing
+        .add(mode, "slicing")
+        .listen()
+        .onChange((value) => {
+          mode.slicing = value;
+          this.setCamera();
+        });
+      slicing.add(this, "saveImage").name("export");
+      slicing.open();
+
+      const planeY = gui.addFolder("planeY");
+      planeY
+        .add(params.planeY, "displayHelper")
+        .listen()
+        .onChange((v) => (planeHelpers[0].visible = v));
+      planeY
+        .add(params.planeY, "constant")
+        .min(-1)
+        .max(1)
+        .listen()
+        .onChange((d) => (planes[0].constant = d));
+      planeY.add(params.planeY, "negated").onChange(() => {
+        planes[0].negate();
+        params.planeY.constant = planes[0].constant;
+      });
+      planeY.open();
+
+      const scale = gui.addFolder("scale");
+      scale
+        .add(params, "scale")
+        .min(0)
+        .max(1)
+        .listen()
+        .onChange((d) => {
+          params.scale = d;
+        });
+      scale.open();
+    },
+
     init: function () {
       container = document.querySelector("#app");
       this.setScene();
-      this.setCamera();
       // TODO 3 более прозрачные границы
       planes = [
         new THREE.Plane(new THREE.Vector3(0, -1, 0), params.planeY.constant),
@@ -56,46 +158,15 @@ export default {
         new THREE.Plane(new THREE.Vector3(0, 0, -1), 1),
         new THREE.Plane(new THREE.Vector3(0, 0, -1), -1).negate(),
       ];
-
       planeHelpers = planes.map((p) => new THREE.PlaneHelper(p, 2, 0xffffff));
       planeHelpers.forEach((ph) => {
         ph.visible = true;
         scene.add(ph);
       });
-      this.setGUI();
       this.setMaterial();
       this.createLights();
       this.setRender();
-    },
-
-    setGUI: function () {
-      const gui = new GUI();
-      gui.add(params, "animate");
-
-      const planeY = gui.addFolder("planeY");
-      planeY
-        .add(params.planeY, "displayHelper")
-        .onChange((v) => (planeHelpers[0].visible = v));
-      planeY
-        .add(params.planeY, "constant")
-        .min(-1)
-        .max(1)
-        .onChange((d) => (planes[0].constant = d));
-      planeY.add(params.planeY, "negated").onChange(() => {
-        planes[0].negate();
-        params.planeY.constant = planes[0].constant;
-      });
-      planeY.open();
-
-      const scale = gui.addFolder("scale");
-      scale
-        .add(params, "scale")
-        .min(0)
-        .max(1)
-        .onChange((d) => {
-          params.scale = d;
-        });
-      scale.open();
+      this.setCamera();
     },
 
     setScene: function () {
@@ -104,19 +175,38 @@ export default {
     },
 
     setCamera: function () {
-      camera = new THREE.PerspectiveCamera(
-        35,
-        container.clientWidth / container.clientHeight,
-        0.1,
-        100
-      );
-      camera.position.set(0, 0, 10);
-    },
-
-    loadGeometry: async function () {
-      return await loader.loadAsync(
-        "models/Model_44_S3.540.45_T3.8.46_E4.3.47_R5.7.stl"
-      );
+      if (mode.slicing) {
+        camera = new THREE.OrthographicCamera(-2, 2, 1, -1);
+        camera.position.set(0, 2, 0);
+        camera.lookAt(new THREE.Vector3(0, 1, 0));
+        planeHelpers.forEach((ph) => {
+          ph.visible = false;
+          // const normal = ph.plane.normal;
+          // if (normal.y === -1 || normal.y === 1) {
+          //   ph.visible = false;
+          // } else {
+          //   console.log(ph.plane.normal);
+          // }
+          // if (ph.normal) {
+          //   console.log(ph.plane.normal);
+          //   ph.visible = false;
+          // }
+        });
+        object.children[7].visible = false;
+      } else {
+        camera = new THREE.PerspectiveCamera(
+          35,
+          container.clientWidth / container.clientHeight,
+          0.1,
+          100
+        );
+        camera.position.set(0, 0, 10);
+        planeHelpers.forEach((ph) => {
+          ph.visible = true;
+        });
+        object.children[7].visible = true;
+        new OrbitControls(camera, renderer.domElement);
+      }
     },
 
     setMaterial: function () {
@@ -138,7 +228,7 @@ export default {
 
         // plane is clipped by the other clipping planes
         const planeMat = new THREE.MeshStandardMaterial({
-          color: 0xe91e63,
+          color: "white", // 0xe91e63
           metalness: 0.1,
           roughness: 0.75,
           clippingPlanes: planes.filter((p) => p !== plane),
@@ -222,14 +312,17 @@ export default {
         5 // intensity
       );
 
-      const mainLight = new THREE.DirectionalLight(0xffffff, 5);
-      mainLight.position.set(10, 10, 10);
+      // const mainLight = new THREE.DirectionalLight(0xffffff, 5);
+      // mainLight.position.set(10, 10, 10);
 
-      scene.add(ambientLight, mainLight);
+      scene.add(ambientLight);
     },
 
     setRender: function () {
-      renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer = new THREE.WebGLRenderer({
+        // antialias: true, // убрал, поскольку оставляет неприятную лесенку на картинке
+        preserveDrawingBuffer: true,
+      });
       renderer.setSize(container.clientWidth, container.clientHeight);
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.physicallyCorrectLights = true;
